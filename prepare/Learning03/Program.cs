671841bfb0b8de70937e33fa6f524b9b68c59fcf
NateGFoster <nate.fosty@gmail.com>
2025-05-23 17:13:47 -0600

    assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Gimme a number");
+        Console.ReadLine();
+        Fraction fraction = new Fraction();
+
+        Console.WriteLine("Gimme a number");
+        string a = Console.ReadLine();
+        int input = int.Parse(a);
+        Fraction fraction_1 = new Fraction(input);
+
+        Console.WriteLine("Gimme a number");
+        string top = Console.ReadLine();
+        int input_2 = int.Parse(top);
+        string bottom = Console.ReadLine();
+        int input_3 = int.Parse(bottom);
+        Fraction fraction_2 = new Fraction(input_2, input_3);
+
+
+        //fraction_1.GetFractrionString();
+        Console.WriteLine(fraction_1.GetFractrionString());
+        //fraction_2.GetDecimalValue();
+        Console.WriteLine(  fraction_2.GetDecimalValue());
     }
+   
 }
---
+++ prepare/Learning03/whateverelse/fraction.cs
@@ -0,0 +1,47 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public Fraction()
+    {
+
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void SetBottom(int Bottom)
+    {
+        _top = Bottom;
+    }
+    public string GetFractrionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+      public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
+ 
